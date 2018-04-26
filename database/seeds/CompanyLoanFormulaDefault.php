3d66650c4e0bae8a588bfdfedddfa326ba942a99
Waldi Irawan <waldiirawan127@gmail.com>
2018-04-26 14:18:15 +0700

    add seed the default formula

---
+++ database/seeds/CompanyLoanFormulaDefault.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\CompanyLoanFormula;
+
+use Illuminate\Database\Seeder;
+
+class CompanyLoanFormulaDefault extends Seeder
+{
+    protected $formula = null;
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->formula = (object)[
+            'oncepaid' => (object)[
+                'capped' => 10,
+                'interest' => 1
+            ],
+            'installments' => (object)[
+                'interest' => 1
+            ],
+            'default' => true,
+        ];
+
+        if (CompanyLoanFormula::where('default', true)->count() < 1) {
+            $CompanyLoanFormula = new CompanyLoanFormula();
+            $CompanyLoanFormula->oncepaid = $this->formula->oncepaid;
+            $CompanyLoanFormula->installments = $this->formula->installments;
+            $CompanyLoanFormula->default = true;
+            $CompanyLoanFormula->save();
+        }
+    }
+}
